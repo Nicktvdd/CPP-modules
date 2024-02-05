10422f5f6132b105a9eda8ce090fb17ab1a3a52a
nicktvdd <nicktvdd@gmail.com>
2024-02-05 08:27:10 +0200

    ex05

---
+++ ex05/src/main.cpp
@@ -0,0 +1,15 @@
+#include "../inc/Harl.hpp"
+
+int main ()
+{
+    Harl harl;
+
+    harl.complain("debug");
+    harl.complain("info");
+    harl.complain("warning");
+    harl.complain("error");
+
+    harl.complain("");
+    harl.complain("Hello");
+    return (0);
+}
