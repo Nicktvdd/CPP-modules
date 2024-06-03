d845ca27e35d9784a70ba0559859c56cfbf47134
Nick van den Dungen <Nicktvdd@hotmail.com>
2024-06-03 12:04:40 +0000

    Comments for readability

--- module06/ex00/ScalarConverter.hpp
+++ module06/ex00/ScalarConverter.hpp
@@ -23,23 +23,23 @@ private:
     static int scType; //1 == char, 2 == int, 3 == float, 4 == double, 5 == TROUBLE :(
 
     // Private functions
-    static int detectType(std::string);
-    static void castOthers(void);
-    static void printVariables(void);
-    static int isChar(std::string);
-    static int isInt(std::string);
-    static int isFloat(std::string);
-    static int isDouble(std::string);
+    static int detectType(std::string); // Detects the type of the input string
+    static void castOthers(void); // Casts the input string to other types
+    static void printVariables(void); // Prints the converted variables
+    static int isChar(std::string); // Checks if the input string can be converted to char
+    static int isInt(std::string); // Checks if the input string can be converted to int
+    static int isFloat(std::string); // Checks if the input string can be converted to float
+    static int isDouble(std::string); // Checks if the input string can be converted to double
 
     // Private constructors and destructor
-    ScalarConverter(void);
-    ScalarConverter(const ScalarConverter& other);
-    ScalarConverter& operator=(const ScalarConverter& other);
-    ~ScalarConverter(void);
+    ScalarConverter(void); // Default constructor
+    ScalarConverter(const ScalarConverter& other); // Copy constructor
+    ScalarConverter& operator=(const ScalarConverter& other); // Assignment operator
+    ~ScalarConverter(void); // Destructor
 
 public:
     // Public function
-    static int convert(std::string str);
+    static int convert(std::string str); // Converts the input string to different types
 };
 
 #endif
